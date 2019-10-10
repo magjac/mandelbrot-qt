b38ceb6a60eadd834110c4f54966913e87514b3f
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2019-10-10 10:46:54 +0200

    Replace empty main window with colorful plot on graphics scene

--- main.cpp
+++ main.cpp
@@ -1,11 +1,29 @@
-#include "mainwindow.h"
 #include <QApplication>
+#include <QPushButton>
+#include <QGraphicsScene>
+#include <QGraphicsView>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    QGraphicsScene scene (0, 0, 1024, 512);
+    QGraphicsView view(&scene);
+    view.show();
 
+    const int width = 1024;
+    const int height = 512;
+    QPixmap pixmap(width, height);
+    QPainter p(&pixmap);
+
+    for (int ir = 0; ir < height; ir++)
+    {
+        for (int ic = 0; ic < width; ic++)
+        {
+            QColor color (ir % 256, ic % 256, ir % 256);
+            p.setPen(color);
+            p.drawPoint(ic, ir);
+        }
+    }
+    scene.addPixmap(pixmap);
     return a.exec();
 }
